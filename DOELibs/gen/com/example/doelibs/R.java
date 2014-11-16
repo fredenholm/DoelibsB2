b4e1248aecf93d2b518df28207ce51e4be7e74e3
Nidaa <nidaa_albotany@yahoo.com>
2014-11-16 04:07:10 +0100

    Adding LoginActivity, RegisterActivity similar to those in mockups. Styling The spinner and the Switch. The Login/Register launches from the Action Bar

--- DOELibs/gen/com/example/doelibs/R.java
+++ DOELibs/gen/com/example/doelibs/R.java
@@ -16,6 +16,9 @@ public final class R {
         public static final int abc_slide_out_bottom=0x7f040004;
         public static final int abc_slide_out_top=0x7f040005;
     }
+    public static final class array {
+        public static final int staff=0x7f0c0000;
+    }
     public static final class attr {
         /**  Custom divider drawable to use for elements in the action bar. 
          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
@@ -1082,7 +1085,7 @@ containing a value of this type.
         public static final int search_icon=0x7f020059;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f090044;
+        public static final int LinearLayout1=0x7f09004b;
         public static final int action_bar=0x7f09001c;
         public static final int action_bar_activity_content=0x7f090001;
         public static final int action_bar_container=0x7f09001b;
@@ -1091,16 +1094,17 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f090023;
         public static final int action_bar_title=0x7f090022;
         public static final int action_context_bar=0x7f09001d;
-        public static final int action_createbook=0x7f090049;
+        public static final int action_createbook=0x7f090050;
+        public static final int action_loginRegister=0x7f090051;
         public static final int action_menu_divider=0x7f090002;
         public static final int action_menu_presenter=0x7f090003;
         public static final int action_mode_close_button=0x7f090024;
-        public static final int action_scan=0x7f090047;
-        public static final int action_settings=0x7f090048;
+        public static final int action_scan=0x7f09004e;
+        public static final int action_settings=0x7f09004f;
         public static final int activity_chooser_view_content=0x7f090025;
         public static final int always=0x7f09000f;
         public static final int beginning=0x7f090016;
-        public static final int browse_searchImageView=0x7f090046;
+        public static final int browse_searchImageView=0x7f09004d;
         public static final int button1=0x7f090042;
         public static final int checkbox=0x7f09002d;
         public static final int collapseActionView=0x7f090010;
@@ -1108,7 +1112,12 @@ containing a value of this type.
         public static final int dialog=0x7f090014;
         public static final int disableHome=0x7f090009;
         public static final int dropdown=0x7f090015;
-        public static final int edit=0x7f090045;
+        public static final int edit=0x7f09004c;
+        public static final int editConfirmPassword=0x7f090048;
+        public static final int editEmail=0x7f090043;
+        public static final int editFirstName=0x7f090046;
+        public static final int editLastName=0x7f090047;
+        public static final int editPassword=0x7f090044;
         public static final int editText1=0x7f09003d;
         public static final int editText2=0x7f09003e;
         public static final int editText3=0x7f09003f;
@@ -1130,10 +1139,11 @@ containing a value of this type.
         public static final int never=0x7f090012;
         public static final int none=0x7f090019;
         public static final int normal=0x7f090007;
-        public static final int pager=0x7f090043;
+        public static final int pager=0x7f090045;
         public static final int progress_circular=0x7f090004;
         public static final int progress_horizontal=0x7f090005;
         public static final int radio=0x7f09002f;
+        public static final int roleSwitch=0x7f090049;
         public static final int search_badge=0x7f090032;
         public static final int search_bar=0x7f090031;
         public static final int search_button=0x7f090033;
@@ -1148,6 +1158,7 @@ containing a value of this type.
         public static final int showCustom=0x7f09000b;
         public static final int showHome=0x7f09000c;
         public static final int showTitle=0x7f09000d;
+        public static final int spin=0x7f09004a;
         public static final int split_action_bar=0x7f09001e;
         public static final int submit_area=0x7f090039;
         public static final int tabMode=0x7f090008;
@@ -1208,15 +1219,19 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_create_book=0x7f030018;
-        public static final int activity_main=0x7f030019;
-        public static final int activity_search=0x7f03001a;
-        public static final int fragment_browse=0x7f03001b;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001c;
+        public static final int activity_login=0x7f030019;
+        public static final int activity_main=0x7f03001a;
+        public static final int activity_register=0x7f03001b;
+        public static final int activity_search=0x7f03001c;
+        public static final int fragment_browse=0x7f03001d;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001e;
     }
     public static final class menu {
-        public static final int create_book=0x7f0c0000;
-        public static final int main=0x7f0c0001;
-        public static final int search=0x7f0c0002;
+        public static final int create_book=0x7f0d0000;
+        public static final int login=0x7f0d0001;
+        public static final int main=0x7f0d0002;
+        public static final int register=0x7f0d0003;
+        public static final int search=0x7f0d0004;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1264,17 +1279,29 @@ containing a value of this type.
         public static final int addbook=0x7f0a001c;
         public static final int app_name=0x7f0a000d;
         public static final int author=0x7f0a0019;
+        public static final int choose_role_staff_student=0x7f0a0028;
+        public static final int confirm_password=0x7f0a0027;
         public static final int description=0x7f0a001b;
+        public static final int email=0x7f0a0026;
+        public static final int first_name=0x7f0a0024;
         public static final int hello_world=0x7f0a000e;
         public static final int information=0x7f0a001d;
         /**  Create Book Activity 
          */
         public static final int isbn=0x7f0a0017;
+        public static final int last_name=0x7f0a0025;
+        public static final int log_in_register=0x7f0a001f;
+        public static final int login_register=0x7f0a0020;
         public static final int publication_year=0x7f0a001a;
+        public static final int register=0x7f0a002a;
+        public static final int register_account=0x7f0a0023;
         public static final int scan=0x7f0a001e;
+        public static final int selct_staff_to_vouch_you=0x7f0a0029;
         public static final int title=0x7f0a0018;
         public static final int title_account_section=0x7f0a0015;
         public static final int title_activity_create_book=0x7f0a0016;
+        public static final int title_activity_login=0x7f0a0021;
+        public static final int title_activity_register=0x7f0a0022;
         public static final int title_activity_search=0x7f0a0011;
         /**  TABS 
          */
